16110aad94df2caf0e0b9819ca26961d9fba0b45
Josué Oviedo <josovi28@gmail.com>
2022-06-14 22:46:51 -0600

    first lection python

---
+++ variables/practica/practic#1.py
@@ -0,0 +1,11 @@
+#pedir 2 datos al usuario y mostrarlos
+#de los dos datos hacer una operacion y mostrar el resultado
+namev="Bienvenido"
+print(type(namev))
+print(namev)
+op1=int(input("seleccione un numero:"))
+op2=int(input("selecciuone otro:"))
+
+operacion=op1+op2
+print(f'operacion={operacion}')
+
