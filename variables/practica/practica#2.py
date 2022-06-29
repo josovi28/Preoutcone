76a62368e31a9026f38d7a5286b7721e11c92f27
Josué Oviedo <josovi28@gmail.com>
2022-06-28 21:48:58 -0600

    Aprendiendo en free code camp

--- variables/practica/practica#2.py
+++
@@ -1,8 +0,0 @@
-#pedir 2 dato
-#si son iguales tirar que son inguales y si no, pos que no
-valor1=int(input("Ingrese el primer valor:"))
-valor2=int(input("Ingrese el segundo valor:"))
-if valor1==valor2:
-    print("Son iguales")
-else:
-    print("No son iguales")    
